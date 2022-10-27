b59927c7314d38cfa421b5d97ba07f7f0c5ec70d
*Ajayi <*emmanueltemitayo056@gmail.com*>
2022-10-27 22:34:33 +0100

    Project Set up complete and views created

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+require("dotenv").config();
+const db = require("./db");
+
+const PORT = process.env.PORT;
+const app = express();
+
+app.use(express.json())
+
+// db.connectToDb();
+
+app.set('views', 'views')
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render("index")
+})
+
+app.get('/login', (req, res) => {
+    res.render("login")
+})
+
+app.get('/signup', (req, res) => {
+    // res.send("You have registered successfully")
+    res.render('signup')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on https://localhost:${PORT}/`)
+})
---
+++ models/userModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const UserSchema = new Schema({
+  id: ObjectId,
+  firstname: {
+      type: String,
+      required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true,
+    min: 0,
+    max: 11
+},
+  email: {
+      type: String,
+      required: true
+  },
+  phonenumber: {
+    type: String,
+    required: true
+  },
+  password: { 
+      type: String,
+      required: true,
+      min: 8
+  }
+})
+
+
+const User = mongoose.model('User', UserSchema)
+module.exports = User;
